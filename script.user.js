3e5cf57e8fc2aa37ae2db2b76cf6ed306587e649
JenieX <sozorablack@gmail.com>
2023-09-29 07:18:59 +0300

    feat: add examples

--- script.user.js
+++ script.user.js
@@ -12,8 +12,49 @@
 // @icon           https://letterboxd.com/favicon.ico
 // ==/UserScript==
 
+/** @typedef {0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10} FilmRating */
+/** @typedef {{ name: string; id: string; rating: FilmRating }} UserFilm */
+
 async function main() {
-  console.log('Hello!');
+  const user = 'rksO0';
+  const filmId = 'head-to-head-2023-1';
+
+  // ------------------------
+
+  console.log(`%cFetching ${user}'s friends..`, 'color: #00ffbb');
+
+  /** @type {string[]} */
+  const friends = await letterboxdScrapper.fetchFriends(user);
+
+  console.log(friends);
+
+  // ------------------------
+
+  console.log(`%cFetching ${user}'s films..`, 'color: #00ffbb');
+
+  /** @type {UserFilm[]} */
+  const films = await letterboxdScrapper.fetchFilms({
+    type: 'default',
+    user,
+    rating: '5',
+    genre: 'action',
+    sort: 'name',
+  });
+
+  console.log(films);
+
+  // ------------------------
+
+  console.log(`%cFetching ${filmId} users..`, 'color: #00ffbb');
+
+  /** @type {string[]} */
+  const members = await letterboxdScrapper.fetchMembers({
+    type: 'members',
+    id: filmId,
+    rating: '5',
+  });
+
+  console.log(members);
 }
 
 main().catch((exception) => {
